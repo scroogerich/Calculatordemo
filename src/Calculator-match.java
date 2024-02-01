a0aa466a9ae8080c4373a656b2f0ffde64bbae01
scroogerich <wd.richh@gmail.com>
2024-02-01 12:23:50 +0300

    Initial commit

---
+++ src/Calculator-match.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Калькулятор {
+    public static void main(String[] args) {
+        System.out.println("Введите математическое задание (например, 2 + 2):");
+
+        Scanner input = new Scanner(System.in);
+        String expression = input.nextLine();
+
+        System.out.println(calc(expression));
+    }
+
+    public static String calc(String input) {
+        String[] expression = input.split(" ");
+        if (expression.length != 3) {
+            throw new ArithmeticException("Строка не является математической операцией!");
+        }
+
+        int operandOne = Integer.parseInt(expression[0]);
+        char operator = expression[1].charAt(0);
+        int operandTwo = Integer.parseInt(expression[2]);
+
+        int sum = 0;
+
+        if ((operandOne > 0 && operandTwo > 0) && (operandOne < 11 && operandTwo < 11)) {
+            switch (operator) {
+                case '+':
+                    sum = operandOne + operandTwo;
+                    break;
+                case '-':
+                    sum = operandOne - operandTwo;
+                    break;
+                case '*':
+                    sum = operandOne * operandTwo;
+                    break;
+                case '/':
+                    sum = operandOne / operandTwo;
+                    break;
+                default:
+                    throw new ArithmeticException("Используем только 1 из перечисленных операторов +, -, /, *");
+            }
+        } else {
+            throw new ArithmeticException("По условию используем операнды от 1 до 10 включительно!");
+        }
+        return String.valueOf(sum);
+    }
+}
+
